b75817f30cd01262af137f2201bf414d17252197
Peps@ <352481@mail.ru>
2024-02-09 19:34:43 +0300

    Выполнено первое задание.

--- main.py
+++ main.py
@@ -5,10 +5,25 @@
 Реализуйте конструктор по умолчанию и метод для вывода данных
 """
 # Решение задачи №1
-class Human:
-    ...
+# class Human:
+#     def __init__(self, name: str, data: str, phone: int, city: str, country: str, adress: str):
+#         self.name = name
+#         self.data = data
+#         self.phone = phone
+#         self.city = city
+#         self.country = country
+#         self.adress = adress
+#
+#     def __str__(self):
+#         return (f'ФИО: {self.name} \n '
+#                 f'Дата рождения: {self.data}\n'
+#                 f' Телефон: {self.phone}\n'
+#                 f' Город: {self.city}\n'
+#                 f' Страна: {self.country}\n'
+#                 f' Адрес: {self.adress}')
 
-
+# chelovek = Human("Петр Яковлевич Бессмертных", '9 апреля 1984 г.', 352481, 'Ярославль', 'РОССИЯ', 'Volkova street')
+# print(chelovek)
 
 """Задание 2
 Реализуйте класс "Книга" в полях класса:
