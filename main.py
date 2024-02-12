216a613174650f64e03218ede3d9d0e0e7ce0ef8
Peps@ <352481@mail.ru>
2024-02-12 19:41:55 +0300

    Задания готовы к проверке.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 домашний адрес.
 Реализуйте конструктор по умолчанию и метод для вывода данных
 """
-# Решение задачи №1
+#Решение задачи №1
 # class Human:
 #     def __init__(self, name: str, data: str, phone: int, city: str, country: str, adress: str):
 #         self.name = name
@@ -17,12 +17,12 @@
 #     def __str__(self):
 #         return (f'ФИО: {self.name} \n '
 #                 f'Дата рождения: {self.data}\n'
-#                 f' Телефон: {self.phone}\n'
-#                 f' Город: {self.city}\n'
-#                 f' Страна: {self.country}\n'
-#                 f' Адрес: {self.adress}')
-
-# chelovek = Human("Петр Яковлевич Бессмертных", '9 апреля 1984 г.', 352481, 'Ярославль', 'РОССИЯ', 'Volkova street')
+#                 f'Телефон: {self.phone}\n'
+#                 f'Город: {self.city}\n'
+#                 f'Страна: {self.country}\n'
+#                 f'Адрес: {self.adress}')
+#
+# chelovek = Human("Петр Яковлевич Бессмертных", '9 апреля 1984 г.', 352481, 'Ярославль', 'РОССИЯ', 'Ул. Свободы 1/2')
 # print(chelovek)
 
 """Задание 2
@@ -30,22 +30,23 @@
 название книги, год выпуска, издателя, жанр, автора, цену
 Реализуйте конструктор по умолчанию и метод для вывода данных
 """
-class book:
-    def __init__(self, name: str, year: int, copyright: str, genre: str, autor: str, price: int):
-        self.name = name
-        self.year = year
-        self.copyright = copyright
-        self.genre = genre
-        self.autor = autor
-        self.price = price
-
-    def __str__(self):
-        return (f'Название книги: {self.name}\n'
-                f' Год выпуска: {self.year}\n'
-                f' Издатель: {self.copyright}\n'
-                f' Жанр: {self.genre} \n'
-                f'Автор: {self.autor} \n'
-                f'Цена книги: {self.price}')
-book_1 = book('Властелин колец', 2002, 'ООО Печать', 'Fantasy', 'R.R.Tolkin', 99 )
-print(book_1)
+       # Решение задачи № 2
+# class book:
+#     def __init__(self, name: str, year: int, copyright: str, genre: str, autor: str, price: float):
+#         self.name = name
+#         self.year = year
+#         self.copyright = copyright
+#         self.genre = genre
+#         self.autor = autor
+#         self.price = price
+#
+#     def __str__(self):
+#         return (f'Название книги: {self.name}\n'
+#                 f'Год выпуска: {self.year}\n'
+#                 f'Издатель: {self.copyright}\n'
+#                 f'Жанр: {self.genre} \n'
+#                 f'Автор: {self.autor} \n'
+#                 f'Цена книги: {self.price}')
+# book_1 = book('The Lord of Rings', 2002, 'ООО Печать', 'Fantasy', 'R.R.Tolkin', 999.99 )
+# print(book_1)
 
